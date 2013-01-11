051310e0fa09a06d48a13a1a5ee90c4ba6f19785
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2013-01-11 12:02:33 +0000

    reg-registered distribution

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
     name = "ris-distrib-test",
     url='https://github.com/pp-mo/iris_distribution_test',
-    download_url = 'https://github.com/pp-mo/iris_distribution_test/archive/idt-0.0.2.zip',
+    download_url = 'https://github.com/pp-mo/iris_distribution_test/archive/master.zip',
     version = "0.0.2",
     packages = ['iris_distribution_test'],
     author='ppmo',
