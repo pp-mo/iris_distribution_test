b93ef4d83d43c6c616f27d6a11c561d4ddaf5f59
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2013-01-11 13:18:33 +0000

    new version 0.1.2

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ setup(
     url='https://github.com/pp-mo/iris_distribution_test',
     download_url = 
       'https://github.com/pp-mo/iris_distribution_test/blob/master/dist/iris-distrib-test-0.0.2.tar.gz',
-    version = "0.0.2",
+    version = "0.1.2",
     packages = ['iris_distribution_test'],
     author='ppmo',
     author_email='avd-support@metoffice.gov.uk',
 )
+
