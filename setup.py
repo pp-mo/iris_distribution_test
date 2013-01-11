15c88b70460f0a438031dc8543812a0425d31570
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2013-01-11 12:11:17 +0000

    change download link to embedded dist tar

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup
 setup(
     name = "ris-distrib-test",
     url='https://github.com/pp-mo/iris_distribution_test',
-    download_url = 'https://github.com/pp-mo/iris_distribution_test/archive/master.zip',
+    download_url = 
+      'https://github.com/pp-mo/iris_distribution_test/blob/master/dist/ris-distrib-test-0.0.2.tar.gz',
     version = "0.0.2",
     packages = ['iris_distribution_test'],
     author='ppmo',
